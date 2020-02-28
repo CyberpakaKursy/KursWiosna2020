45aee27e12a38619d3730fabe1aff44cae3d6d63
Maelren <mbohatkiewicz@corelogic.pl>
2020-02-29 00:19:49 +0100

    Zmiana na nowej galezi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Kurs wiosenny czas zacząć!");
+        System.out.println("Kurs wiosenny czas zacząć!\nW tym semestrze bedzie wiecej zadan domowych i nauki!");
     }
 }
