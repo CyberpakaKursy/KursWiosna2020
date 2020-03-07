afa217029d6cd2ca239c6912e3f8026b6ff686a8
CyberPaka3 <mvaiery823@gmail.com>
2020-03-07 16:09:48 +0100

    Costam

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,9 @@ public class Main {
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-            String formatDateTime = now.format(formatter);
-            System.out.println("Otworzono o:\t" + formatDateTime);
-            String znakNaKoniecPentli = scanner.nextLine();
+//            String formatDateTime = now.format(formatter);
+//            System.out.println("Otworzono o:\t" + formatDateTime);
+//            String znakNaKoniecPentli = scanner.nextLine();
             if (znakNaKoniecPentli.equals("q")){
                 break;
             }
