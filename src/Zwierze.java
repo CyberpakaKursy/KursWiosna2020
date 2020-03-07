7f14c939b972b4b159ef9a3bff5598ed1da7978f
CyberPaka KN <mcj.luter@gmail.com>
2020-03-07 16:23:38 +0100

    Moj nowy commit-Magdulka

--- src/Zwierze.java
+++ src/Zwierze.java
@@ -2,6 +2,7 @@ public interface Zwierze {
     void IloscOwiec(int liczba);
     String LokalizaciaOwcy();
     void KolorOwcy(String kolorowcy);
+    String mojaNowaWspanialaZmiana();
     Integer Wiekowcy(int Wiek);
 
     void tak(int tak);
