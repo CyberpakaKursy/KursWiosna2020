f43cc48ac5a2aa2c6ca93b1f5bb435a89caf3184
CJ <mcj.luter@gmail.com>
2020-03-28 14:29:00 +0100

    dzielenie zrobione

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,7 +1,15 @@
 public class Kalkulator {
 
 	public String mnozenie(double a, double b){}
-	public String dzielenie(double a, double b){}
+	public String dzielenie(double a, double b){
+		Double wynik;
+		wynik = Double.valueOf(0);
+		if(b==0) {
+			System.out.println(" nie mozesz dxielic przez 0");
+		}else wynik = a/b;
+
+		return wynik.toString();
+	}
 	public String odejmowanie(double a, double b){}
 	public String dodawania(double a, double b){}
 	public String potegowanie(double a, double b){}
