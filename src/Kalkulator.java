d37041e014f0906af18cea591ab7c862ecf39340
unknown <jarekmacko75@gmail.com>
2020-03-28 14:28:32 +0100

    dodawanie kalkulator

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -3,6 +3,9 @@ public class Kalkulator {
 	public String mnozenie(double a, double b){}
 	public String dzielenie(double a, double b){}
 	public String odejmowanie(double a, double b){}
-	public String dodawania(double a, double b){}
+	public String dodawania(double a, double b){
+		double wynik = a + b;
+		return String.valueOf(wynik);
+	}
 	public String potegowanie(double a, double b){}
 }
