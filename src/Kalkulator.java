cb6e976944d72ac7a66e0913105fd5678d919dcf
Val Mushenyk <mvaiery823@gmail.com>
2020-03-28 14:38:16 +0100

    mnozenie od val

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,6 +1,9 @@
 public class Kalkulator {
 
-	public String mnozenie(double a, double b){}
+	public String mnozenie(double a, double b){
+		double wynikmnozenia = a*b;
+		return String.valueOf(wynikmnozenia);
+	}
 	public String dzielenie(double a, double b){}
 	public String odejmowanie(double a, double b){}
 	public String dodawania(double a, double b){}
