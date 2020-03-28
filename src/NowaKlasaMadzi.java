79bd4fbd6a242f2669d737908a659486290ab738
Magda_Majcher <magda.majcher03@gmail.com>
2020-03-28 13:59:05 +0100

    kommit madzi

---
+++ src/NowaKlasaMadzi.java
@@ -0,0 +1,11 @@
+public class NowaKlasaMadzi {
+    public static void madzia() {
+        System.out.println("tu Madzia");
+    }
+    public static void madzia2() {
+        System.out.println("tu Madzia ponownie");
+    }
+    public static void madzia3() {
+        System.out.println("tu Madzia po raz trzeci");
+    }
+}
