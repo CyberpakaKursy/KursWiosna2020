63a222cbc4e59001fd9af7100a57334f26f99d65
JarekMacko <jarekmacko75@gmail.com>
2020-03-28 14:31:17 +0100

    poprawa nicku

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -8,6 +8,7 @@ public class Kalkulator {
 	public String odejmowanie(double a, double b){}
 	public String dodawania(double a, double b){
 		double wynik = a + b;
+
 		return String.valueOf(wynik);
 	}
 	public String potegowanie(double a, double b){}
