a7eb489e1e6e45ce4a0918c94b4464e1f8eb6e25
Kingbuster9903 <chrisdab867@gmail.com>
2020-03-28 14:29:13 +0100

    Chris Komit Kalkulator

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -4,5 +4,13 @@ public class Kalkulator {
 	public String dzielenie(double a, double b){}
 	public String odejmowanie(double a, double b){}
 	public String dodawania(double a, double b){}
-	public String potegowanie(double a, double b){}
+	public String potegowanie(double a, double b){
+		//a do potęgi b.
+		double c = a;
+		for (int i=0; i<b; i++){
+			c=c*a;
+		}
+		String wynik = Double.toString(c);
+		return wynik;
+	}
 }
