d53de810d11771b1b01b0e2a547469c82eff4a34
Magda_Majcher <magda.majcher03@gmail.com>
2020-03-28 14:30:16 +0100

    nowy commit Madzia

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -2,7 +2,10 @@ public class Kalkulator {
 
 	public String mnozenie(double a, double b){}
 	public String dzielenie(double a, double b){}
-	public String odejmowanie(double a, double b){}
+	public String odejmowanie(double a, double b){
+		double wynik= a - b;
+		return String.valueOf(wynik);
+	}
 	public String dodawania(double a, double b){}
 	public String potegowanie(double a, double b){}
 }
