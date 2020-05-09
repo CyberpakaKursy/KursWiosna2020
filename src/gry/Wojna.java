bb2d4dc305289cfc3733205f890628e409027e74
Magda_Majcher <magda.majcher03@gmail.com>
2020-05-09 13:56:36 +0200

    Nowy commit Madzi

--- src/gry/Wojna.java
+++ src/gry/Wojna.java
@@ -55,9 +55,47 @@ public class Wojna implements Gra {
         listaKart.add(new Karta("6Kier",6));
         /*7*/
         listaKart.add(new Karta("7Pik",7));
-        listaKart.add(new Karta("8Pik",8));
-        listaKart.add(new Karta("9Pik",9));
-        listaKart.add(new Karta("10Pik",10));
+        listaKart.add(new Karta("7Trefl",7));
+        listaKart.add(new Karta("7Karo",7));
+        listaKart.add(new Karta("7Kier",7));
+        /*8*/
+        listaKart.add(new Karta("8Pik",7));
+        listaKart.add(new Karta("8Trefl",7));
+        listaKart.add(new Karta("8Karo",7));
+        listaKart.add(new Karta("8Kier",7));
+        /*9*/
+        listaKart.add(new Karta("9Pik",7));
+        listaKart.add(new Karta("9Trefl",7));
+        listaKart.add(new Karta("9Karo",7));
+        listaKart.add(new Karta("9Kier",7));
+        /*10*/
+        listaKart.add(new Karta("10Pik",7));
+        listaKart.add(new Karta("10Trefl",7));
+        listaKart.add(new Karta("10Karo",7));
+        listaKart.add(new Karta("10Kier",7));
+        /*jupek*/
+        listaKart.add(new Karta("JupekPik",11));
+        listaKart.add(new Karta("JupekTrefl",11));
+        listaKart.add(new Karta("JupekKaro",11));
+        listaKart.add(new Karta("JupekKier",11));
+        /*dama*/
+        listaKart.add(new Karta("DamaPik",12));
+        listaKart.add(new Karta("DamaTrefl",12));
+        listaKart.add(new Karta("DamaKaro",12));
+        listaKart.add(new Karta("DamaKier",12));
+        /*krol*/
+        listaKart.add(new Karta("KrolPik",13));
+        listaKart.add(new Karta("KrolTrefl",13));
+        listaKart.add(new Karta("KrolKaro",13));
+        listaKart.add(new Karta("KrolKier",13));
+        /*AS*/
+        listaKart.add(new Karta("ASPik",14));
+        listaKart.add(new Karta("ASTrefl",14));
+        listaKart.add(new Karta("ASKaro",14));
+        listaKart.add(new Karta("ASKier",14));
+        /*Jocker*/
+        listaKart.add(new Karta("JockerCzerwony",15));
+        listaKart.add(new Karta("JockerCzarny",15));
 
     }
 
