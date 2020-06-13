ae5fa6aeff79f2a6cd46b5822ff3ecca936cb971
JarekMacko <jarekmacko75@gmail.com>
2020-06-13 14:39:44 +0200

    BlackJack13

--- src/gry/BlackJack/BlackJack.java
+++ src/gry/BlackJack/BlackJack.java
@@ -29,94 +29,94 @@ public class BlackJack implements Gra {
     public void ustawPoczatkoweWartosci() {
         Random rand = new Random();
         talia = new ArrayList<>();
-        HashSet<Karta> listaKart = new HashSet<>();
         /*2*/
-        listaKart.add(new Karta("2Pik", 2));
-        listaKart.add(new Karta("2Trefl", 2));
-        listaKart.add(new Karta("2Karo", 2));
-        listaKart.add(new Karta("2Kier", 2));
+        talia.add(new Karta("2Pik", 2));
+        talia.add(new Karta("2Trefl", 2));
+        talia.add(new Karta("2Karo", 2));
+        talia.add(new Karta("2Kier", 2));
         /*3*/
-        listaKart.add(new Karta("3Pik", 3));
-        listaKart.add(new Karta("3Trefl", 3));
-        listaKart.add(new Karta("3Karo", 3));
-        listaKart.add(new Karta("3Kier", 3));
+        talia.add(new Karta("3Pik", 3));
+        talia.add(new Karta("3Trefl", 3));
+        talia.add(new Karta("3Karo", 3));
+        talia.add(new Karta("3Kier", 3));
         /*4*/
-        listaKart.add(new Karta("4Pik", 4));
-        listaKart.add(new Karta("4Trefl", 4));
-        listaKart.add(new Karta("4Karo", 4));
-        listaKart.add(new Karta("4Kier", 4));
+        talia.add(new Karta("4Pik", 4));
+        talia.add(new Karta("4Trefl", 4));
+        talia.add(new Karta("4Karo", 4));
+        talia.add(new Karta("4Kier", 4));
         /*5*/
-        listaKart.add(new Karta("5Pik", 5));
-        listaKart.add(new Karta("5Trefl", 5));
-        listaKart.add(new Karta("5Karo", 5));
-        listaKart.add(new Karta("5Kier", 5));
+        talia.add(new Karta("5Pik", 5));
+        talia.add(new Karta("5Trefl", 5));
+        talia.add(new Karta("5Karo", 5));
+        talia.add(new Karta("5Kier", 5));
         /*6*/
-        listaKart.add(new Karta("6Pik", 6));
-        listaKart.add(new Karta("6Trefl", 6));
-        listaKart.add(new Karta("6Karo", 6));
-        listaKart.add(new Karta("6Kier", 6));
+        talia.add(new Karta("6Pik", 6));
+        talia.add(new Karta("6Trefl", 6));
+        talia.add(new Karta("6Karo", 6));
+        talia.add(new Karta("6Kier", 6));
         /*7*/
-        listaKart.add(new Karta("7Pik", 7));
-        listaKart.add(new Karta("7Trefl", 7));
-        listaKart.add(new Karta("7Karo", 7));
-        listaKart.add(new Karta("7Kier", 7));
+        talia.add(new Karta("7Pik", 7));
+        talia.add(new Karta("7Trefl", 7));
+        talia.add(new Karta("7Karo", 7));
+        talia.add(new Karta("7Kier", 7));
         /*8*/
-        listaKart.add(new Karta("8Pik", 8));
-        listaKart.add(new Karta("8Trefl", 8));
-        listaKart.add(new Karta("8Karo", 8));
-        listaKart.add(new Karta("8Kier", 8));
+        talia.add(new Karta("8Pik", 8));
+        talia.add(new Karta("8Trefl", 8));
+        talia.add(new Karta("8Karo", 8));
+        talia.add(new Karta("8Kier", 8));
         /*9*/
-        listaKart.add(new Karta("9Pik", 9));
-        listaKart.add(new Karta("9Trefl", 9));
-        listaKart.add(new Karta("9Karo", 9));
-        listaKart.add(new Karta("9Kier", 9));
+        talia.add(new Karta("9Pik", 9));
+        talia.add(new Karta("9Trefl", 9));
+        talia.add(new Karta("9Karo", 9));
+        talia.add(new Karta("9Kier", 9));
         /*10*/
-        listaKart.add(new Karta("10Pik", 10));
-        listaKart.add(new Karta("10Trefl", 10));
-        listaKart.add(new Karta("10Karo", 10));
-        listaKart.add(new Karta("10Kier", 10));
+        talia.add(new Karta("10Pik", 10));
+        talia.add(new Karta("10Trefl", 10));
+        talia.add(new Karta("10Karo", 10));
+        talia.add(new Karta("10Kier", 10));
         /*jupek*/
-        listaKart.add(new Karta("JupekPik", 2));
-        listaKart.add(new Karta("JupekTrefl", 2));
-        listaKart.add(new Karta("JupekKaro", 2));
-        listaKart.add(new Karta("JupekKier", 2));
+        talia.add(new Karta("JupekPik", 2));
+        talia.add(new Karta("JupekTrefl", 2));
+        talia.add(new Karta("JupekKaro", 2));
+        talia.add(new Karta("JupekKier", 2));
         /*dama*/
-        listaKart.add(new Karta("DamaPik", 3));
-        listaKart.add(new Karta("DamaTrefl", 3));
-        listaKart.add(new Karta("DamaKaro", 3));
-        listaKart.add(new Karta("DamaKier", 3));
+        talia.add(new Karta("DamaPik", 3));
+        talia.add(new Karta("DamaTrefl", 3));
+        talia.add(new Karta("DamaKaro", 3));
+        talia.add(new Karta("DamaKier", 3));
         /*krol*/
-        listaKart.add(new Karta("KrolPik", 4));
-        listaKart.add(new Karta("KrolTrefl", 4));
-        listaKart.add(new Karta("KrolKaro", 4));
-        listaKart.add(new Karta("KrolKier", 4));
+        talia.add(new Karta("KrolPik", 4));
+        talia.add(new Karta("KrolTrefl", 4));
+        talia.add(new Karta("KrolKaro", 4));
+        talia.add(new Karta("KrolKier", 4));
         /*AS*/
-        listaKart.add(new Karta("ASPik", 11));
-        listaKart.add(new Karta("ASTrefl", 11));
-        listaKart.add(new Karta("ASKaro", 11));
-        listaKart.add(new Karta("ASKier", 11));
-
-
-        ArrayList<Karta> talia = new ArrayList<>();
-        Random random = new Random();
-        int iloscPrzetasowan = 30 + random.nextInt(30);
-
-        for (int i = 0; i < iloscPrzetasowan; i++) ;
-        {
-                int miejscaPierwsze = random.nextInt(talia.size());
-                Karta kartaZJakiegosRandomowegoMiejsca = talia.get(miejscaPierwsze);
-                int miejsceDrugie = random.nextInt(talia.size());
-                Karta kartaZJakiegosDrugiegoRandomowegoMiejsca = talia.get(miejsceDrugie);
-
-                talia.remove(miejscaPierwsze);
-                talia.add(miejsceDrugie, kartaZJakiegosRandomowegoMiejsca);
-                talia.remove(miejsceDrugie);
-                talia.add(miejscaPierwsze, kartaZJakiegosDrugiegoRandomowegoMiejsca);
-
-            }
-
+        talia.add(new Karta("ASPik", 11));
+        talia.add(new Karta("ASTrefl", 11));
+        talia.add(new Karta("ASKaro", 11));
+        talia.add(new Karta("ASKier", 11));
+        
+        talia = tasowanie(talia);
     }
+    
+    
+    private ArrayList tasowanie(ArrayList<Karta> talia) {
+        Random random = new Random();
+        int iloscPrzetasowan = 40 + random.nextInt(30);
+        for (int i = 0; i < iloscPrzetasowan; i++) {
+            int miejscaPierwsze = random.nextInt(talia.size());
+            Karta kartaZJakiegosRandomowegoMiejsca = talia.get(miejscaPierwsze);
+            int miejsceDrugie = random.nextInt(talia.size());
+            Karta kartaZJakiegosDrugiegoRandomowegoMiejsca = talia.get(miejsceDrugie);
 
+            talia.remove(miejscaPierwsze);
+            talia.add(miejsceDrugie, kartaZJakiegosRandomowegoMiejsca);
+            talia.remove(miejsceDrugie);
+            talia.add(miejscaPierwsze, kartaZJakiegosDrugiegoRandomowegoMiejsca);
+
+        }
+        return talia;
+    }
+    
     @Override
     public boolean rozpocznijGre() {
         boolean koniecGry = false;
